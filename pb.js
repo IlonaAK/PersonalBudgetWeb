4ac0fdf68f48875e6d9a554937bea010ff31afae
Ilona Lelek <lelek.ilona.programista@gmail.com>
2022-11-26 21:19:20 +0100

    Add page for incomes and validate all files

---
+++ pb.js
@@ -0,0 +1,13 @@
+var data= new Date();
+
+
+
+const date=document.getElementById('date');
+const example=date.getAttribute('placeholder');
+example = "getFullYear()+"-"+(getMonth()+1)+"-"+getDate()";
+
+
+function wyswietlDate()
+{
+	document.getElementById("new")=toLocaleStringString();
+}
