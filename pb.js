f4b97e6a54df141d9f3d4d146410267a77a8ecf7
Ilona Lelek <lelek.ilona.programista@gmail.com>
2022-11-27 16:03:00 +0100

    Change names of pages to English, create pages for expenses and balance

--- pb.js
+++ pb.js
@@ -1,13 +1,17 @@
-var data= new Date();
+function currentDate()
+	{
+		var today = new Date();
+		
+		var day = today.getDate();
+		if (day<10) day = "0"+day;
+		
+		var month = today.getMonth()+1;
+		if (month<10) month= "0"+month;
+		
+		var year = today.getYear();
+		var t=year + "-" + month + "-" + day;
 
-
-
-const date=document.getElementById('date');
-const example=date.getAttribute('placeholder');
-example = "getFullYear()+"-"+(getMonth()+1)+"-"+getDate()";
-
-
-function wyswietlDate()
-{
-	document.getElementById("new")=toLocaleStringString();
+         const divDate = document.getElementById('daten').innerHTML;
+		divDate.setAttribute(placeholder,t);
 }
+   
